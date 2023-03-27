1971a12addd7ba34e335038d4be2ca2e4728a3d4
Satyamjha24 <pananrocks18@gmail.com>
2023-03-27 16:40:21 +0530

    backend2

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const { connection } = require('./db')
 const { Auth } = require('./middleware/auth.middleware')
 const { postRouter } = require('./routes/post.route')
 const { userRouter } = require('./routes/user.route')
+const cors=require('cors')
+require('dotenv').config()
 
 const app=express()
+
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req,res)=>{
